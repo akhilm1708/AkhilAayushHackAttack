682173ef3c73e2a34034da68501d27e819e34514
akhilMuthyala <119759995+akhilm1708@users.noreply.github.com>
2024-11-21 09:58:28 -0800

    Added functions
    for POST, GET and INC

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# import requests
+
+# Creating the endpoint
+#  import requests
 
 # #change the userName to your name
 # userName = "AkhilAayush"
@@ -21,25 +23,99 @@
 # print(response2.text)
 
 
+
+
+
+
+
+# #Post Request
+# import requests
+
+# url = "https://cs-api.pltw.org/AkMuthy"
+# pString = "?text="
+# textToPost = "Hello"
+# response = requests.post(url + pString + textToPost)
+# print(response.text)
+# print(response.status_code)
+
+
+# print(url + pString + textToPost)
+
+
+
+# #Get Request
+# import requests
+
+# url = "https://cs-api.pltw.org/"
+
+# userName = "AkMuthy"
+
+# response = requests.get(url + userName)
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+
+# #Increment code
+# import requests
+
+# url = "https://cs-api.pltw.org/AkMuthy/increment?id="
+# num = str(2)
+
+# for _ in range(3):
+#     response = requests.post(url+num)
+
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+
+
+# # Interview Post
+
 import requests
+global url
+API = "https://cs-api.pltw.org/"
+user = input("Which user would you like to hack:")
 
-url = "https://cs-api.pltw.org/"
 
-userName = "AkhilAayush"
+url = API + user
 
-# Construct the endpoint for the API request
-pString = "string/?text="
-textToPost = "Hello world"
 
-# Send a POST request to the endpoint
-response = requests.post(url + userName + "/" + pString + textToPost)
+def INC():
+    id = "increment?id"
 
-# Check the status code to ensure the request was successful
-print("Status Code:", response.status_code)
 
-# Print the headers of the response (may include useful information like content type, etc.)
-print("Response Headers:", response.headers)
+    num  = input("What index would you like to change:")
+    count = input("what number would you like to change it to:")
 
-# Print the response text (this should be the data returned by the API)
-print("Response Body:", response.text)
+
+    for _ in range (count):
+        inc_response = requests.post(url+id+num)
+
+
+    print(inc_response.status_code)
+    print(inc_response.headers)
+    print(inc_response.text)
+
+
+def POST():
+    pString = "?text="
+    my_text = input("What do you want to post:")
+    post_response = requests.post(url+pString+my_text)
+
+
+    print(post_response.status_code)
+    print(post_response.headers)
+    print(post_response.text)
+
+
+for n in range(10):
+    POST()
+
+    
+
+
+
+
+
+
 
